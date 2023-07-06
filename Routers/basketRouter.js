1a055b3868ee6f2b232aca7fe411374ce96da50c
Murad Gojayev <muradgojayevv@gmail.com>
2023-07-06 11:48:32 +0400

    total price

--- Routers/basketRouter.js
+++ Routers/basketRouter.js
@@ -7,112 +7,117 @@ const router = express.Router();
 
 // localhost:5000/basket (POST)
 
-router.post('/set-item', authenticateToken, async (req, res) => {
-    try {
-        const productId = req.body.productId;
-        const userId = req.data.user;
+router.post("/set-item", authenticateToken, async (req, res) => {
+  try {
+    const productId = req.body.productId;
+    const userId = req.data.user;
 
-        const basketExist = await Basket.findOne({ userId: userId })
+    const basketExist = await Basket.findOne({ userId: userId });
 
+    if (basketExist) {
+      const index = basketExist.items.findIndex(
+        (item) => item.product == productId
+      );
 
-        if (basketExist) {
-            const index = basketExist.items.findIndex(item => item.product == productId)
-
-            if (index != -1) {
-                basketExist.items[index].count += 1
-            } else {
-                basketExist.items.push({
-                    product: productId,
-                    count: 1
-                })
-            }
-            await basketExist.save()
-            return res.status(200).json({ data: basketExist.items })
-        }
-
-
-        const basket = await Basket.create({
-            userId: userId,
-            items: [{
-                product: productId,
-                count: 1
-            }]
-        })
-
-        return res.json({ data: basket.items })
-
-    } catch (error) {
-        return res.status(404).json({ message: error.message })
+      if (index != -1) {
+        basketExist.items[index].count += 1;
+      } else {
+        basketExist.items.push({
+          product: productId,
+          count: 1,
+        });
+      }
+      await basketExist.save();
+      return res.status(200).json({ data: basketExist.items });
     }
-})
 
-router.post('/remove-item', authenticateToken, async (req, res) => {
-    try {
+    const basket = await Basket.create({
+      userId: userId,
+      items: [
+        {
+          product: productId,
+          count: 1,
+        },
+      ],
+    });
 
-        const userId = req.data.user;
-        const productId = req.body.productId;
-        const count = req.body.count;
+    return res.json({ data: basket.items });
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+});
 
-        const basket = await Basket.findOne({ userId: userId })
+router.post("/remove-item", authenticateToken, async (req, res) => {
+  try {
+    const userId = req.data.user;
+    const productId = req.body.productId;
+    const count = req.body.count;
 
-        const itemIndex = basket.items.findIndex(item => item.product == productId);
-        if (count == 0) {
-            basket.items.splice(itemIndex, 1)
-        } else {
-            basket.items[itemIndex].count = count;
-        }
-        basket.save();
+    const basket = await Basket.findOne({ userId: userId });
 
-        return res.status(200).json({ data: basket.items })
-    } catch (error) {
-        return res.status(400).json({ message: error.message })
+    const itemIndex = basket.items.findIndex(
+      (item) => item.product == productId
+    );
+    if (count == 0) {
+      basket.items.splice(itemIndex, 1);
+    } else {
+      basket.items[itemIndex].count = count;
     }
-})
+    basket.save();
 
-router.get('/get-items', authenticateToken, async (req, res) => {
-    try {
-        const userId = req.data.user;
+    return res.status(200).json({ data: basket.items });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+});
 
-        const basket = await Basket.findOne({ userId: userId })
+router.get("/get-items", authenticateToken, async (req, res) => {
+  try {
+    const userId = req.data.user;
 
-        if (basket) return res.status(200).json({ data: basket.items })
+    const basket = await Basket.findOne({ userId: userId });
 
-        return res.status(200).json({ data: [] })
+    if (basket) return res.status(200).json({ data: basket.items });
 
-    } catch (error) {
-        return res.status(400).json({ message: error.message })
+    return res.status(200).json({ data: [] });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+});
+
+router.get("/get-products-data", authenticateToken, async (req, res) => {
+  try {
+    const userId = req.data.user;
+    const basket = await Basket.findOne({ userId: userId });
+
+    if (basket) {
+      let list = [];
+
+      basket.items.forEach((element) => {
+        list.push(element.product);
+      });
+
+      const products = await Product.find({ _id: { $in: list } });
+
+      let totalPrice = 0;
+
+      products.forEach((item) => {
+        basket.items.forEach((basktetItem) => {
+          if (item._id.toString() == basktetItem.product.toString()) {
+            totalPrice =
+              totalPrice +
+              basktetItem.count * (item.offer > 0 ? item.offer : item.price);
+          }
+        });
+      });
+
+      return res.status(200).json({ data: products, price: totalPrice });
     }
-})
 
-
-router.get('/get-products-data', authenticateToken, async (req, res) => {
-    try {
-        const userId = req.data.user;
-        const basket = await Basket.findOne({ userId: userId });
-
-        if (basket) {
-
-            let list = [];
-
-            basket.items.forEach(element => {
-                list.push(element.product)
-            });
-
-            const products = await Product.find({ _id: { $in: list } })
-
-
-            return res.status(200).json({ data: products })
-        }
-
-        return 
-
-    } catch (error) {
-        return res.status(400).json({ message: error.message })
-    }
-})
-
-
-
-
+    return;
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+});
 
 export default router;
